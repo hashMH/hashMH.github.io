e94bb28563e8b49d849382119fe28ea43e450b21
Manoj Hans <hash_practo@Manojs-MacBook-Pro.local>
2015-05-28 09:59:53 +0530

    studymate pgpm live

---
+++ studymate_pgpm/js/singleClick.js
@@ -0,0 +1,18 @@
+﻿
+        function ClientSideClick(myButton) {
+            // Client side validation
+            if (typeof (Page_ClientValidate) == 'function') {
+                if (Page_ClientValidate() == false)
+                { return false; }
+            }
+            
+            //make sure the button is not of type "submit" but "button"
+            if (myButton.getAttribute('type') == 'button') {
+                // diable the button
+                myButton.disabled = true;
+                myButton.value = "wait...";  
+                                              
+            }
+            return true;
+        }
+
